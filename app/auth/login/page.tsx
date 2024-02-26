01668653d3d390b194850ef3a4913fcd710f4e6e
Balram Uddhav Melkunde <102988461+BalramMEL@users.noreply.github.com>
2024-02-26 21:29:31 +0530

    Login UI and installation of zod

---
+++ app/auth/login/page.tsx
@@ -0,0 +1,10 @@
+import LoginForm from '@/components/auth/login-form'
+import React from 'react'
+
+const Loginpage = () => {
+  return (
+    <LoginForm />
+  )
+}
+
+export default Loginpage
---
+++ components/auth/Header.tsx
@@ -0,0 +1,27 @@
+import { cn } from "@/lib/utils";
+import { Poppins } from "next/font/google"
+
+
+const font = Poppins({
+    subsets: ["latin"],
+    weight: ["600"],
+})
+
+interface HeaderProps{
+    label: string;
+}
+
+export const Header = ({
+    label,
+}: HeaderProps) => {
+    return (
+        <div className="w-full flex flex-col items-center justify-center gap-y-4">
+            <h1 className={cn("text-3xl font-semibold", font.className)}>
+                🔐Auth
+            </h1>
+            <p className="text-muted-foreground text-sm">
+                {label}
+            </p>
+        </div>
+    )
+}
